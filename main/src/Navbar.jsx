32f503a2f414216e0a23f76955d0de9dc1facf32
Chris <BOBitisme@gmail.com>
2023-01-31 15:49:02 +1300

    Nav bar adapts to mobile

--- main/src/Navbar.jsx
+++ main/src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from "react";
 import { AppBar, Toolbar, Tabs, Tab } from "@mui/material";
+import { isMobile } from "react-device-detect";
 
 const Navbar = (props) => {
     const { page, handlePageChange } = props;
@@ -7,12 +8,12 @@ const Navbar = (props) => {
     const handleChange = useCallback ((event, newValue) => { handlePageChange(newValue); }, [handlePageChange]);
 
     return (
-        <AppBar position="static" component="nav">
+        <AppBar position={isMobile ? "fixed" : "static"} sx={isMobile ? { top: 'auto', bottom: 0 } : null}>
             <Toolbar>
                 <Tabs value={page} onChange={handleChange} aria-label="nav tabs example">
-                  <Tab label="Home" />
-                  <Tab label="Programming" />
-                  <Tab label="Photography" />
+                    <Tab label="Home" />
+                    <Tab label="Programming" />
+                    <Tab label="Photography" />
                 </Tabs>
             </Toolbar>
         </AppBar>
