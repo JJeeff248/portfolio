79c05d41965e91497d710489c3164500b5ad24a1
Chris <BOBitisme@gmail.com>
2023-01-31 22:55:51 +1300

    Reduced image size to look better on mobile

--- main/src/DevPage.jsx
+++ main/src/DevPage.jsx
@@ -15,7 +15,7 @@ const spin = keyframes`
 const DevPage = () => {
     return (
         <Container maxWidth="md" sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "90vh", flexDirection: "column" }}>
-            <Avatar src={logo} alt="logo" variant="rounded" sx={{ width: 400, height: 400, mb: 5, animation: `${spin} infinite 20s linear` }} />
+            <Avatar src={logo} alt="logo" variant="rounded" sx={{ width: 300, height: 300, mb: 5, animation: `${spin} infinite 20s linear` }} />
             <Typography variant="h3" component="h1" fontFamily={ "Courier New, monospace" } fontWeight="bold">
                 Big things coming soon!
             </Typography>
