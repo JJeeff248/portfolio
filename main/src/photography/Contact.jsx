a10a672e5675c741bd5d5f589b3bb631787d08c3
Chris <BOBitisme@gmail.com>
2023-02-11 15:12:57 +1300

    Updated api call for photography contact

--- main/src/photography/Contact.jsx
+++ main/src/photography/Contact.jsx
@@ -2,6 +2,7 @@ import { Alert, Button, IconButton, TextField } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import { isMobile } from "react-device-detect";
+import Axios from 'axios';
 
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -28,7 +29,9 @@ const Contact = () => {
         let valid = name.match(nameRegex);
         valid
             ? setNameError("")
-            : setNameError("Name must be between 2-30 characters and only contain letters, spaces, and hyphens");
+            : setNameError(
+                  "Name must be between 2-30 characters and only contain letters, spaces, and hyphens"
+              );
         return valid;
     };
 
@@ -61,44 +64,50 @@ const Contact = () => {
             return;
         }
 
-        fetch("https://api.chris-sa.com/v1/contact-me", {
+        const data = {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Origin: "https://chris-sa.com",
             },
+            mode: "cors",
             body: JSON.stringify({
                 name: name,
                 email: email,
                 message: message,
             }),
-        })
-            .then((response) => {
-                if (response.status === 200) {
-                    setMessageSent(true);
-                    setAlertOpen(true);
-                    setName("");
-                    setEmail("");
-                    setMessage("");
-                } else {
-                    setMessageSent(false);
-                    setAlertOpen(true);
-                }
-            })
-            .catch((error) => {
+        };
+
+        Axios.post("https://api.chris-sa.com/contact", data).then((response) => {
+            console.log(response);
+            if (response.status === 200) {
+                setMessageSent(true);
+                setAlertOpen(true);
+                setName("");
+                setEmail("");
+                setMessage("");
+            } else {
                 setMessageSent(false);
                 setAlertOpen(true);
-            });
+            }
 
-        setTimeout(() => {
-            setAlertOpen(false);
-        }, 3000);
+            setTimeout(() => {
+                setAlertOpen(false);
+            }, 3000);
+        }).catch((error) => {
+            console.log(error);
+            setMessageSent(false);
+            setAlertOpen(true);
+
+            setTimeout(() => {
+                setAlertOpen(false);
+            }, 3000);
+        });
     };
 
     return (
         <Container
             maxWidth={"lg"}
-            sx={{ mt: 2}}
+            sx={{ mt: 2 }}
             style={{
                 display: "flex",
                 flexDirection: isMobile ? "column" : "row",
