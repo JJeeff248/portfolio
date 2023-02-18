77f1cb93cc4cc9021e294281bab5d4536c3e5ea9
Chris <BOBitisme@gmail.com>
2023-02-18 17:11:05 +1300

    Added about section

--- main/src/dev/DevPage.jsx
+++ main/src/dev/DevPage.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { Avatar, Typography } from "@mui/material";
+import { Box, Container, Divider, Typography } from "@mui/material";
 import { Container, keyframes } from "@mui/system";
 import logo from "../logo.svg";
-
-const spin = keyframes`
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-`;
 
 const DevPage = () => {
     return (
@@ -18,22 +9,51 @@ const DevPage = () => {
             maxWidth="md"
             sx={{
                 display: "flex",
-                justifyContent: "center",
                 alignItems: "center",
                 height: "90vh",
                 flexDirection: "column",
+                mt: 5,
             }}
         >
-            <Avatar
-                src={logo}
-                alt="logo"
-                variant="rounded"
-                sx={{
-                    width: 300,
-                    height: 300,
-                    mb: 5,
-                    animation: `${spin} infinite 20s linear`,
-                }}
+            <Box id="about" width="100%">
+                <Typography variant="h3" textAlign="left" mb={1}>
+                    How did I get here?
+                </Typography>
+                <Typography variant="body1" mb={3}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Eget arcu dictum varius duis at consectetur lorem.
+                    Purus faucibus ornare suspendisse sed nisi lacus sed. Dictum
+                    non consectetur a erat. Mi tempus imperdiet nulla malesuada.
+                    Fermentum posuere urna nec tincidunt. Fermentum odio eu
+                    feugiat pretium nibh ipsum consequat nisl. Sit amet
+                    consectetur adipiscing elit duis. Dignissim suspendisse in
+                    est ante in nibh mauris cursus. Semper eget duis at tellus
+                    at urna condimentum mattis. Lobortis mattis aliquam faucibus
+                    purus in massa tempor nec feugiat. Iaculis urna id volutpat
+                    lacus laoreet non. Turpis massa sed elementum tempus egestas
+                    sed sed risus. Non curabitur gravida arcu ac tortor
+                    dignissim. Aliquam ultrices sagittis orci a scelerisque
+                    purus semper eget. Sit amet aliquam id diam maecenas. Purus
+                    gravida quis blandit turpis cursus in hac habitasse. Ac
+                    feugiat sed lectus vestibulum mattis ullamcorper velit sed
+                    ullamcorper.
+                </Typography>
+                <Typography variant="body1">
+                    Turpis tincidunt id aliquet risus feugiat in ante metus.
+                    Vitae sapien pellentesque habitant morbi tristique senectus
+                    et netus. Viverra orci sagittis eu volutpat odio facilisis
+                    mauris sit amet. Amet nisl purus in mollis nunc sed id
+                    semper risus. Urna nunc id cursus metus aliquam eleifend. Id
+                    diam vel quam elementum pulvinar etiam. Quis ipsum
+                    suspendisse ultrices gravida dictum fusce. Lorem donec massa
+                    sapien faucibus et molestie. Et tortor consequat id porta
+                    nibh venenatis cras sed felis. Egestas dui id ornare arcu
+                    odio. Neque laoreet suspendisse interdum consectetur libero
+                    id faucibus nisl tincidunt. Orci porta non pulvinar neque
+                    laoreet suspendisse interdum consectetur.
+                </Typography>
+            </Box>
             />
             <Typography
                 variant="h3"
